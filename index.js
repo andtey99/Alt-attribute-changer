bf999cc25824adf7e70de16f111c41b6feceb504
andtey99 <apururu@mail.ru>
2023-04-06 21:52:57 +0500

    added comments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const wrapper = document.querySelector("body");
 const button = document.querySelector('button');
 const imgBlock = document.querySelector('.images_block');
 
-button.addEventListener('click', () => {
+button.addEventListener('click', () => {        // handler of "Add" button and adding event listener to handle click on new images
     const image = document.createElement('img');
     image.src = 'http://placekitten.com/g/200/200';
     randomAlt(image);
@@ -11,19 +11,19 @@ button.addEventListener('click', () => {
     imgBlock.appendChild(image);
 })
 
-wrapper.addEventListener('click', () => {
+wrapper.addEventListener('click', () => {       // click on that element removes active border from image
     for (let img of images) {
         img.classList.remove('active');
     }
 });
 
-function randomAlt(elem) {
+function randomAlt(elem) {                     // gives random words for alt attr on images
     fetch("https://random-word-api.herokuapp.com/word?number=1")
         .then(response => response.json())
         .then(result => elem.alt = result);
 }
 
-function handler(img) {
+function handler(img) {                         // img click handler
     const inputField = document.createElement('div');
     inputField.style.cssText = "position: absolute; top: 0; display: flex; flex-direction: column; padding: 5px; z-index: 9999";
     const input = document.createElement('input');
@@ -44,11 +44,11 @@ function handler(img) {
     wrapper.appendChild(inputField);
 }
 
-for (let img of images) {
+for (let img of images) {       // random alt attr for images that already on page
     randomAlt(img);
 }
 
-for (let img of images) {
+for (let img of images) {       // handler for images that already on page
     img.addEventListener('click', (e) => handler(e.target))
     
 }
