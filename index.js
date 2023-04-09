d15c510115763cf3fde43129746b5816275e9365
andtey99 <apururu@mail.ru>
2023-04-10 00:34:56 +0500

    fix: border for all img after random alt attr and make IIFE

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-﻿const images = document.querySelectorAll('img');
+﻿(() => {
+let images = document.querySelectorAll('img');
 const wrapper = document.querySelector("body");
 const button = document.querySelector('button');
 const imgBlock = document.querySelector('.images_block');
+let count = images.length;
+let observer = new MutationObserver(() => {
+    images = document.querySelectorAll('img');
+    for (let i = count; i < images.length; i++) {       // handler for images that already on page
+        randomAlt(images[i]);
+        images[i].addEventListener('click', (e) => handler(e.target))
+    }
+    count = images.length;
+  });
+
+  observer.observe(wrapper, {
+    childList: true,
+    subtree: true,
+    characterDataOldValue: true
+  });
 
 button.addEventListener('click', () => {        // handler of "Add" button and adding event listener to handle click on new images
     const image = document.createElement('img');
     image.src = 'http://placekitten.com/g/200/200';
-    randomAlt(image);
-    image.addEventListener('click', (e) => handler(e.target));
     imgBlock.appendChild(image);
 })
 
 function randomAlt(elem) {                     // gives random words for alt attr on images
     fetch("https://random-word-api.herokuapp.com/word?number=1")
         .then(response => response.json())
-        .then(result => elem.alt = result);
+        .then(result => {
+            elem.style.cssText = "box-shadow: 0px 0px 0px 10px rgb(43, 255, 0);";
+            elem.alt = result
+        });
+        
 }
 
 function handler(img) {                         // img click handler
@@ -26,8 +44,6 @@ function handler(img) {                         // img click handler
     submitButton.textContent = "Изменить";
     submitButton.addEventListener('click', () => {
         img.alt = input.value;
-        img.classList.add('edited');
-        img.style.cssText = "box-shadow: 0px 0px 0px 5px rgb(43, 255, 0);";
         inputField.remove();
     })
     const cancelButton = document.createElement('button');
@@ -45,5 +61,5 @@ for (let img of images) {       // random alt attr for images that already on pa
 
 for (let img of images) {       // handler for images that already on page
     img.addEventListener('click', (e) => handler(e.target))
-    
 }
+})();
